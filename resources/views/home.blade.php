a66801576b7fb71423cec251c78af7be24afb1e0
kavioshanaiesecer <kavi.oshan8@gmail.com>
2017-01-26 21:16:17 +0530

    Login Fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    Hello {{auth::user()->name}}!!!! You are logged in!
                 </div>
 
             </div>
